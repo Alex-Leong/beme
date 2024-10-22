8c621c5847d0fb6229bbae52353de7f3b24cc366
Alex <alexleong.dev@gmail.com>
2024-10-22 16:22:27 +0800

    init project

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    style="
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100vw;
+      height: 100vh;
+    "
+  >
+    <!-- <NuxtRouteAnnouncer /> -->
+    <!-- <NuxtWelcome /> -->
+    <img src="/beme_logo.png" alt="" style="width: 50vw; max-width: 400px" />
+  </div>
+</template>
